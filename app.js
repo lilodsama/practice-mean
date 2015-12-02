cb116f149b22418b2de5994a2e669ddd1c1efed8
lilodsama <lilodsama@gmail.com>
2015-12-02 14:05:38 -0500

    change gitignore

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var api = require('./routes/api');
 
 var app = express();
 
@@ -27,12 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api', api);
 
 // handle all request witch can't be found
-app.all('/*', function(req, res) {
-    res.sendfile('public/index.html');
-});
+// app.all('/*', function(req, res) {
+//     res.sendfile('public/index.html');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
